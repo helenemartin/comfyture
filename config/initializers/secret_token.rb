db11f450026515f3de7150ccd60773e0ea566499
helenemartin <helenemartin13@hotmail.com>
2014-05-11 19:37:06 +0100

    experiment with interface, convenient upload of mp4, wondering how to implement it to existing project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comfy::Application.config.secret_key_base = '2b027d1a98df2bd2f69c39af280849251c21c0fd5fe6a019a2c21d0c6507edf4a90eea7baed5f68e1205727f241490f4427e8c225cf91770281437d7e2d89da9'
